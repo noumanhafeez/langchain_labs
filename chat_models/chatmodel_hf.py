a1d22d67454da61fbce76a4c7344ddb7d9fcdade
noumanhafeez <noumanhafeez095@gmail.com>
2026-06-04 17:47:13 +0500

    feat: add hugging face chat open source model

---
+++ chat_models/chatmodel_hf.py
@@ -0,0 +1,16 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    task="text-generation",
+    max_new_tokens=30
+)
+
+model = ChatHuggingFace(llm=llm)
+
+result = model.invoke("What is the capital of Pakistan?")
+
+print(result)
